12f813e50810103f86e8e9370c14332f5a4c15dd
Vladimir Afonin <vsafonin@gmail.com>
2022-07-13 08:21:02 +0400

    create SQLite dialect class, and create test DB

--- src/main/java/ru/vsafonin/vpnbot/VpnbotApplication.java
+++ src/main/java/ru/vsafonin/vpnbot/VpnbotApplication.java
@@ -8,6 +8,7 @@ public class VpnbotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VpnbotApplication.class, args);
+		System.out.println("i'm alive");
 	}
 
 }
--- src/main/java/ru/vsafonin/vpnbot/sqlite/SQLDialect.java
+++ src/main/java/ru/vsafonin/vpnbot/sqlite/SQLDialect.java
@@ -1,4 +1,119 @@
 package ru.vsafonin.vpnbot.sqlite;
 
-public class SQLDialect {
+import org.hibernate.dialect.Dialect;
+import org.hibernate.dialect.function.SQLFunctionTemplate;
+import org.hibernate.dialect.function.StandardSQLFunction;
+import org.hibernate.dialect.function.VarArgsSQLFunction;
+import org.hibernate.type.StringType;
+
+import java.sql.Types;
+
+public class SQLDialect extends Dialect{
+    public SQLDialect(){
+        registerColumnType(Types.DOUBLE, "double");
+        registerColumnType(Types.VARCHAR, "varchar");
+        registerColumnType(Types.INTEGER, "integer");
+        registerColumnType(Types.BOOLEAN, "integer");
+        registerColumnType(Types.NULL, "null");
+
+        registerFunction("concat", new VarArgsSQLFunction(StringType.INSTANCE, "", "||", ""));
+        registerFunction("mod", new SQLFunctionTemplate(StringType.INSTANCE, "?1 % ?2"));
+        registerFunction("substr", new StandardSQLFunction("substr", StringType.INSTANCE));
+        registerFunction("substring", new StandardSQLFunction("substr", StringType.INSTANCE));
+    }
+
+    public boolean supportsIdentityColumns() {
+        return true;
+    }
+
+    public boolean hasDataTypeInIdentityColumn() {
+        return false; // As specify in NHibernate dialect
+    }
+
+    public String getIdentityColumnString() {
+        // return "integer primary key autoincrement";
+        return "integer";
+    }
+
+    public String getIdentitySelectString() {
+        return "select last_insert_rowid()";
+    }
+
+    public boolean supportsLimit() {
+        return true;
+    }
+
+    protected String getLimitString(String query, boolean hasOffset) {
+        return new StringBuffer(query.length() + 20).append(query).append(hasOffset ? " limit ? offset ?" : " limit ?")
+                .toString();
+    }
+
+    public boolean supportsTemporaryTables() {
+        return true;
+    }
+
+    public String getCreateTemporaryTableString() {
+        return "create temporary table if not exists";
+    }
+
+    public boolean dropTemporaryTableAfterUse() {
+        return false;
+    }
+
+    public boolean supportsCurrentTimestampSelection() {
+        return true;
+    }
+
+    public boolean isCurrentTimestampSelectStringCallable() {
+        return false;
+    }
+
+    public String getCurrentTimestampSelectString() {
+        return "select current_timestamp";
+    }
+
+    public boolean supportsUnionAll() {
+        return true;
+    }
+
+    public boolean hasAlterTable() {
+        return false; // As specify in NHibernate dialect
+    }
+
+    public boolean dropConstraints() {
+        return false;
+    }
+
+    public String getAddColumnString() {
+        return "add column";
+    }
+
+    public String getForUpdateString() {
+        return "";
+    }
+
+    public boolean supportsOuterJoinForUpdate() {
+        return false;
+    }
+
+    public String getDropForeignKeyString() {
+        throw new UnsupportedOperationException("No drop foreign key syntax supported by SQLiteDialect");
+    }
+
+    public String getAddForeignKeyConstraintString(String constraintName, String[] foreignKey, String referencedTable,
+                                                   String[] primaryKey, boolean referencesPrimaryKey) {
+        throw new UnsupportedOperationException("No add foreign key syntax supported by SQLiteDialect");
+    }
+
+    public String getAddPrimaryKeyConstraintString(String constraintName) {
+        throw new UnsupportedOperationException("No add primary key syntax supported by SQLiteDialect");
+    }
+
+    public boolean supportsIfExistsBeforeTableName() {
+        return true;
+    }
+
+    public boolean supportsCascadeDelete() {
+        return false;
+    }
 }
